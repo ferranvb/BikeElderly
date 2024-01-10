ef4b43c6e290d06650336428bd2651563b12abcd
Ferran Vidal <ferranvb@gmail.com>
2024-01-10 13:47:49 +0100

    afegides funcionalitats a Cita Nova

---
+++ src/app/appointments/model/appointment.ts
@@ -0,0 +1,18 @@
+import { Client } from "src/app/clients/model/client";
+import { iVolunteer } from "src/app/volunteers/interface/iVolunteer";
+
+export class Appointment {
+
+  id?: number;
+  day?: Date;
+  startTime?: Date;
+  endTime?: Date;
+  good?: number;
+  volunteer?: iVolunteer;
+  client?: Client;
+  scheduled?: boolean;
+  completed?: boolean;
+
+  constructor() { }
+
+}
--- src/app/appointments/pages/appointments-new/appointments-new.component.ts
+++ src/app/appointments/pages/appointments-new/appointments-new.component.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { InputTextModule } from 'primeng/inputtext';
 import { DatePretyComponent } from '../../components/date-prety/date-prety.component';
 import { AppointmentPanelComponent } from '../../components/appointment-panel/appointment-panel.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { iVolunteer } from 'src/app/volunteers/interface/iVolunteer';
+import { VolunteersService } from '../../../volunteers/volunteers.service';
+interface Good {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'appointments-new',
   standalone: true,
   imports: [
-    CommonModule,ReactiveFormsModule,CalendarModule,InputTextModule,DatePretyComponent,AppointmentPanelComponent
+    CommonModule,ReactiveFormsModule,CalendarModule,InputTextModule,
+    DropdownModule,DatePretyComponent,AppointmentPanelComponent
   ],
   templateUrl: './appointments-new.component.html',
   styleUrl: './appointments-new.component.css',
@@ -18,33 +26,65 @@ import { AppointmentPanelComponent } from '../../components/appointment-panel/ap
 })
 export class AppointmentsNewComponent implements OnInit{ 
 
-  public date: Date = new Date();
+  public date?: Date;
+  
+  public listGoods: Good[] = [
+    {
+    "id": 1,
+    "name": 'Bici Cargo 1'
+    },
+    {
+      "id": 2,
+    "name": 'Bici Cargo 2'
+    }
+  ];
+  public listVolunteers: iVolunteer[] = [];
+  public selectedVolunteer?: iVolunteer;
+  
+  public formAppointment!: FormGroup;
 
-  formCita = new FormControl('');
-  public myForm!: FormGroup;
-
-  constructor(private fb: FormBuilder) {}
+  private volunteersService = inject(VolunteersService);
+  private fb = inject(FormBuilder);
+  constructor() {}
 
   ngOnInit() {
-    this.myForm = this.fb.group({
-      dia: new FormControl('',[
-        Validators.required
-      ],[]),
-      hora: new FormControl('',[
-        Validators.required
-      ],[]),
-      email: new FormControl('', [
-        // validaciones síncronas
-        Validators.required,
-        Validators.email
-      ], [
-        // validaciones asíncronas
-      ]),
-      password: new FormControl('')
+    this.formAppointment = this.fb.group({
+      day: new FormControl('',[Validators.required],[]),
+      good: new FormControl(null,[Validators.required],[]),
+      startTime: new FormControl(null, [Validators.required], []),
+      endTime: new FormControl(null, [Validators.required], []),
+      volunteer:  new FormControl(null),
     })
+
+    this.volunteersService.getVolunteers()
+      .subscribe( res => this.listVolunteers = res
+        
+        );
+    console.log(this.listVolunteers);
+
   }
 
-  public doSomething() {
-    console.log(this.myForm.value);
+  public setDateAppointment():void {
+    this.date = this.formAppointment.value.day;
   }
+
+  public addAppointment():void {
+    console.log("addAppointment", this.formAppointment.value);
+  }
+
+  resetAppointment(): void {
+    this.formAppointment.reset();
+  }
+
+  
 }
+
+// id?: number;
+//   day?: Date;
+//   startTime?: Date;
+//   endTime?: Date;
+//   good?: number;
+//   volunteer?: iVolunteer;
+//   client?: Client;
+//   scheduled?: boolean;
+//   completed?: boolean;
