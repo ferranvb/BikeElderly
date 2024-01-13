3298a2562955eb7eb048142726ab7d050f736253
Ferran Vidal <ferranvb@gmail.com>
2024-01-13 20:00:57 +0100

    Login form creada. Falta lligar en autenticació. I els Guard

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,38 +1,93 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Route, Router, RouterModule } from '@angular/router';
-import { CardModule } from 'primeng/card';
-import { User } from '../../interface/user';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
-    CommonModule, RouterModule,CardModule,ReactiveFormsModule,InputTextModule,ButtonModule 
+    CommonModule, PanelModule, RouterModule,ReactiveFormsModule,InputTextModule,ButtonModule,DialogModule
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
 
-  private user?: User;
+  public loginForm!: FormGroup;
+  public submitted = false;
+  visible: boolean = false;
 
-  constructor(private authService:AuthService, private router: Router) {}
+  // private user?: User;
 
-  loginForm = new FormGroup({
-    user: new FormControl('',[Validators.required]),
-    password:new FormControl('',[Validators.required]),
-  });
+  constructor(private authService:AuthService, 
+    private formBuilder: FormBuilder,
+    private router: Router) 
+  {}
+
+  ngOnInit(): void {
+  
+    this.loginForm = this.formBuilder.group({
+      user: ["", [Validators.required]],
+      password: [
+        "",
+        [
+          Validators.required,
+          // Validators.pattern(
+          //   "(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>\"'\\;:{\\}\\[\\]\\|\\+\\-\\=\\_\\)\\(\\)\\`\\/\\\\\\]])[A-Za-z0-9d$@].{7,}"
+          // )
+        ]
+      ]
+    });
+
+    localStorage.clear();
+  }
 
   onLogin(): void {
-    console.log('Data login' , this.loginForm.value.user);
+    // console.log(this.loginForm.value);
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      localStorage.setItem("user-Data", JSON.stringify(this.loginForm.value));
+      
+      if ( "fer" === this.loginForm.value.user!  ) {
+        this.router.navigate(["/app"]);
+      } else {
+        // this.router.navigate(["/error"]);
+        this.showDialog();
+        this.ngOnInit();
+      }
+    }
+  }
 
-    this.router.navigate(['/app']);
+  
+
+  showDialog() {
+      this.visible = true;
+  }
+
+  // loginForm = new FormGroup({
+  //   user: new FormControl('',[Validators.required]),
+  //   password:new FormControl('',[Validators.required]),
+  // });
+
+  // onLogin(): void {
+  //   console.log('Data login' , this.loginForm.value.user);
+
+  //   //TODO determinar com guardo l'autenticació
+  //   localStorage.setItem('user',this.loginForm.value.user!);
+
+  //   if ( "fer" == localStorage.getItem('user') ) {
+  //     this.router.navigate(['/app']);
+  //   } else {
+  //     this.router.navigate(['error']);
+  //   }
     // this.authService.login(this.loginForm.value.user!, this.loginForm.value.password!)
     //   .subscribe( user => {
     //     this.user = user;
@@ -46,7 +101,7 @@ export class LoginPageComponent {
     //   }
 
      
-  }
+  // }
 
   // @Input()
   // set id(userId: number) {
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { PanelModule } from 'primeng/panel';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'shared-header',
   standalone: true,
   imports: [
-    CommonModule,PanelModule,AvatarModule,AvatarGroupModule,SidebarModule,
+    CommonModule,PanelModule,AvatarModule,AvatarGroupModule,SidebarModule,ButtonModule
+
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
@@ -18,76 +22,9 @@ import { SidebarModule } from 'primeng/sidebar';
 
 export class HeaderComponent {
 
-  selectedItem?: string;
-  visible!: boolean;
-  sidebarVisible: boolean = false;
-
-
-  public toggleMenu(){}
-
-  public logout() {}
-  // user: User;
-
-  // displayNotifications: boolean;
-
-  // notifications?: notification[];
-
-  // constructor(
-  //   private router: Router,
-  //   private routeStateService: RouteStateService,
-  //   private sessionService: SessionService,
-  //   private userIdle: UserIdleService,
-  //   private themeService: ThemeService,
-  //   private userContextService: UserContextService,
-  //   private menuDataService: MenuDataService) {
-
-  //   this.displayNotifications = false;
-
-  //   var selectedTheme = this.sessionService.getItem("selected-theme");
-  //   if (selectedTheme) {
-  //     this.selectTheme(selectedTheme);
-  //   }
-  // }
-
-  // ngOnInit() {
-  //   this.user = this.sessionService.getItem("currentUser");
-  //   this.notifications = [];
-  //   for (var i = 1; i <= 5; i++) {
-  //     var notificationObj = new notification("Message " + i, new Date(), null)
-  //     this.notifications.push(notificationObj);
-  //   }
-
-  //   //Start watching for user inactivity.
-  //   this.userIdle.startWatching();
-
-  //   // Start watching when user idle is starting.
-  //   this.userIdle.onTimerStart().subscribe();
-
-  //   // Start watch when time is up.
-  //   this.userIdle.onTimeout().subscribe(() => {
-  //     this.logout();
-  //   });
-  // }
-
-  // logout() {
-  //   this.userIdle.stopWatching();
-  //   this.routeStateService.removeAll();
-  //   this.userContextService.logout();
-  //   this.sessionService.removeItem('active-menu');
-  //   this.router.navigate(['/login']);
-  // }
-
-  // showNotificationSidebar() {
-  //   this.displayNotifications = true;
-  // }
-
-  // toggleMenu() {
-  //   this.menuDataService.toggleMenuBar.next(true);
-  // }
-
-  // selectTheme(theme: string) {
-  //   this.sessionService.setItem("selected-theme", theme);
-  //   this.themeService.selectTheme(theme);
-  // }
-
+  private router = inject(Router);
+  
+  public onLogout() {
+    this.router.navigate(['/']);
+  }
 }
