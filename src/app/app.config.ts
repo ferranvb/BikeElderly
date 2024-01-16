cdfc893c8961e1b642a6dd6b01fed16cd83672fb
Ferran Vidal <ferranvb@gmail.com>
2024-01-16 23:26:42 +0100

    Barallant-me amb el LOCALE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import {routes} from './app.routes';
 import {provideAnimations} from "@angular/platform-browser/animations";
 import { HttpClientModule, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { LOCALE_ID } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +15,7 @@ export const appConfig: ApplicationConfig = {
       withFetch(),
     ),
     // provideHttpClient(withInterceptorsFromDi()),
- 
+    // [{provide: LOCALE_ID, useValue: 'es-ES' }],
     MessageService]
 };
 
